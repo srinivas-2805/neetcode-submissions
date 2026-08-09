ba8f9c0dc81f4e37ae9058f198f7ef25a3127a5e
srinivas <bhukyasrinivasarao140@gmail.com>
2026-08-09 15:52:53 +0545

    Add: non-cyclical-number - submission-0

---
+++ Data Structures & Algorithms/non-cyclical-number/submission-0.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isHappy(int n) {
+        int slow=n;
+        int fast=n;
+        do{
+            slow=findsquare(slow);
+            fast=findsquare(findsquare(fast));
+        } while(slow!=fast);
+        if(slow==1){
+            return true;
+        }
+        return false;
+    }
+    public int findsquare(int n){
+        int ans=0;
+        while(n>0){
+          int rem=n%10;
+          ans+=rem*rem;
+          n=n/10;
+        }
+        return ans;
+    }
+}
