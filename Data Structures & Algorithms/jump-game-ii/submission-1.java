14e87aa5e4668147799427c545310e5efa6979bd
srinivas <bhukyasrinivasarao140@gmail.com>
2026-08-21 23:24:49 +0545

    Add: jump-game-ii - submission-1

---
+++ Data Structures & Algorithms/jump-game-ii/submission-1.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int jump(int[] nums) {
+        int totaljumps=0;
+        int destination=nums.length-1;
+        int coverage=0,lastjumpidx=0;
+        if(nums.length==1) return 0;
+        for(int i=0;i<nums.length;i++){
+            coverage=Math.max(coverage,i+nums[i]);
+            if(i==lastjumpidx){
+                lastjumpidx=coverage;
+                totaljumps++;
+                if(coverage>=destination){
+                    return totaljumps;
+                }
+            }
+        }
+        return totaljumps;
+    }
+}
